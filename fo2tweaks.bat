b40109b7b3da48bfe5771a4119e1c36de9728b0c
Ilya Ivanov <ilya@devopsx.com>
2016-10-29 05:28:09 +0600

    log main bat errors

--- fo2tweaks.bat
+++ fo2tweaks.bat
@@ -1,6 +1,6 @@
 @echo off
-attrib -r data/proto/items/* /s /d
-attrib -r data/proto/critters/* /s /d
+attrib -r data/proto/items/* /s /d >> fo2tweaks\fo2tweaks.bat.log 2>&1
+attrib -r data/proto/critters/* /s /d >> fo2tweaks\fo2tweaks.bat.log 2>&1
 fo2tweaks\tools\weidu.exe fo2tweaks\fo2tweaks.tp2 --nogame --log fo2tweaks\fo2tweaks.log
-attrib +r data/proto/items/* /s /d
-attrib +r data/proto/critters/* /s /d
+attrib +r data/proto/items/* /s /d >> fo2tweaks\fo2tweaks.bat.log 2>&1
+attrib +r data/proto/critters/* /s /d >> fo2tweaks\fo2tweaks.bat.log 2>&1
