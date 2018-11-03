551e62fa1abcd002999eaeceb4efafa45aa93498
Magus <magus@bgforge.net>
2018-11-04 00:28:07 +0700

    streamline code

--- fo2tweaks/source/headers/fo2tweaks/fo2tweaks.h
+++ fo2tweaks/source/headers/fo2tweaks/fo2tweaks.h
@@ -2,8 +2,10 @@
 #include "..\sfall\sfall.h"
 #include "..\sfall\lib.arrays.h"
 #include "..\sfall\lib.strings.h"
+#include "..\sfall\define_extra.h"
 #include "tile.h"
 
+#define NAME "fo2tweaks"
 #define fo2tweaks_ini "fo2tweaks.ini"
 #define fo2tweaks_setting(section, setting) get_ini_setting(fo2tweaks_ini + "|" + section + "|" + setting)
 #define fo2tweaks_string(section, setting) get_ini_string(fo2tweaks_ini + "|" + section + "|" + setting)
@@ -77,10 +79,6 @@
 #define in_combat (get_game_mode bwand COMBAT)
 #define is_dead(critter_obj) (critter_state(critter_obj) == CRITTER_IS_DEAD)
 
-procedure max(variable x, variable y) begin
-  if x > y then return x;
-  return y;
-end
 
 procedure get_active_weapon(variable attacker) begin
   variable item;
@@ -160,4 +158,31 @@ end
 procedure in_party(variable obj) begin
   if is_in_array(obj, party_member_list_critters) then return true;
   else return false;
+end
+
+procedure is_empty(variable container_or_critter_obj) begin
+  if inven_ptr(container_or_critter_obj, 0) then return false;
+  return true;
+end
+
+procedure is_container(variable obj) begin
+  if obj_type(obj) != OBJ_TYPE_ITEM then return false;
+  if obj_item_subtype(obj) != item_type_container then return false;
+  return true;
+end
+
+procedure get_party_skill_level(variable skill) begin
+  variable who;
+  variable who_best;
+  variable skill_level := 0;
+  variable skill_name;
+  foreach who in party_member_list_critters begin
+    variable who_skill := critter_skill_level(who, skill);
+    if who_skill > skill_level then begin
+      skill_level := who_skill;
+      who_best := who;
+    end
+  end
+  ndebug(obj_name(who_best) + " has best skill " + mstr_skill(skill_name) + " of " + skill_level);
+  return [who_best, skill_level];
 end
