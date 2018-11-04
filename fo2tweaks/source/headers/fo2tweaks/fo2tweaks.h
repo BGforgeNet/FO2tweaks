f771b124ecf91d428a6667e92e469f8e555b5bde
Magus <magus@bgforge.net>
2018-11-05 00:08:48 +0700

    massive code cleanup, using better functions

--- fo2tweaks/source/headers/fo2tweaks/fo2tweaks.h
+++ fo2tweaks/source/headers/fo2tweaks/fo2tweaks.h
@@ -12,55 +12,6 @@
 #define ndebug(message) debug_msg(NAME + ": " + message + "\n")
 
 #define sec_main "main"
-#define set_carry_weight "carry_weight"
-#define set_grenades_chance "grenades_chance"
-#define set_level5 "level5"
-#define set_worldmap_key "worldmap_key"
-#define set_molotov_fire "molotov_fire"
-#define set_no_knockback "no_knockback"
-#define set_trunk_space "trunk_space"
-#define set_autodoors "autodoors"
-#define set_ammobox "ammobox"
-#define set_sneak_spotted "sneak_spotted"
-#define set_modoc_brahmin "modoc_brahmin"
-#define set_healing_revision "healing_revision"
-#define set_no_scope_penalty "no_scope_penalty"
-#define set_hp_over_head "hp_over_head"
-#define set_party_skill_items "party_skill_items"
-#define set_damage_mod "damage_mod"
-
-#define sec_damage_mod "damage_mod"
-#define set_damage_mod_new_burst "new_burst"
-#define set_damage_mod_random_bodyparts "unaimed_shots_random_bodyparts"
-#define set_damage_mod_bodyparts_chance "unaimed_shots_bodyparts_chance"
-
-#define sec_party_skill_items "party_skill_items"
-
-#define sec_sneak_spotted "sneak_spotted"
-#define set_sneak_spotted_msg "message"
-#define set_sneak_spotted_stop "stop"
-
-#define sec_ammobox "ammobox"
-#define set_ammobox_number "number"
-#define set_ammobox_color "color"
-
-#define sec_radon "broken_radon"
-#define set_radon_armor "armor"
-#define set_radon_items "items"
-
-#define sec_grenades_chance "grenades_chance"
-#define set_grenades_weapons "weapons"
-#define set_grenades_ammo "ammo"
-
-#define sec_molotov_fire "molotov_fire"
-#define set_molotov_flameboy "flameboy"
-
-#define sec_worldmap "worldmap"
-#define set_worldmap_hotkey "hotkey"
-
-#define sec_no_scope_penalty "no_scope_penalty"
-#define set_scope_remove "remove"
-#define set_scope_to_long "to_long"
 
 #define is_weapon(x) (obj_item_subtype(x) == item_type_weapon)
 #define is_critter(obj)   (obj_type(obj) == OBJ_TYPE_CRITTER)
