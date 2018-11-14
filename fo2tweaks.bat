9724205b2d58febe4ca28f9003337e369cfabd82
Magus <magus@bgforge.net>
2018-11-14 22:44:21 +0700

    remove old components

--- fo2tweaks.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-attrib -r data/proto/items/* /s /d >> fo2tweaks\fo2tweaks.bat.log 2>&1
-attrib -r data/proto/critters/* /s /d >> fo2tweaks\fo2tweaks.bat.log 2>&1
-fo2tweaks\tools\weidu.exe fo2tweaks\fo2tweaks.tp2 --nogame --log fo2tweaks\fo2tweaks.log
-attrib +r data/proto/items/* /s /d >> fo2tweaks\fo2tweaks.bat.log 2>&1
-attrib +r data/proto/critters/* /s /d >> fo2tweaks\fo2tweaks.bat.log 2>&1
