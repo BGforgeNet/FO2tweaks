cb4de37ac74d6c9a98d4878c8b8afd512f4b128e
Magus <magus@bgforge.net>
2023-08-02 07:13:21 +0700

    fix import error

--- extra/env.sh
+++ extra/env.sh
@@ -13,3 +13,15 @@ export WINEDEBUG=-all
 export mod_name=fo2tweaks
 
 set +xeu +o pipefail
+
+# git magic to clone a subdirectory, for faster build
+function git-clone-dir() {
+    url="$1"
+    dir="$2"
+    subdir="$3"
+
+    git clone -n --depth=1 --filter=tree:0 "$url" "$dir"
+    cd "$dir"
+    git sparse-checkout set --no-cone "$subdir"
+    git checkout
+}
